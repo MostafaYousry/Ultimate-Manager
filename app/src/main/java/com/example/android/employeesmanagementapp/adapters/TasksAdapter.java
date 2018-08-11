64da866fe198278acd9add0badbc793031bbf203
Moamen <moamenmohamedalii@gmail.com>
2018-08-11 15:23:11 +0200

    add start and due dates in task item , add popup menu for task options

--- app/src/main/java/com/example/android/employeesmanagementapp/adapters/TasksAdapter.java
+++ app/src/main/java/com/example/android/employeesmanagementapp/adapters/TasksAdapter.java
@@ -2,16 +2,20 @@ package com.example.android.employeesmanagementapp.adapters;
 
 
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.example.android.employeesmanagementapp.R;
 import com.example.android.employeesmanagementapp.data.entries.TaskEntry;
+import com.example.android.employeesmanagementapp.utils.AppUtils;
 
 import java.util.List;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.widget.PopupMenu;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class TasksAdapter extends RecyclerView.Adapter<TasksAdapter.TasksViewHolder> {
@@ -70,17 +74,51 @@ public class TasksAdapter extends RecyclerView.Adapter<TasksAdapter.TasksViewHol
 
     class TasksViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         View mItemView;
-        TextView mTextView;
+        TextView mTaskTitle;
+        TextView mTaskStartDate;
+        TextView mTaskDueDate;
+        ImageButton mTaskOptions;
 
         TasksViewHolder(View itemView) {
             super(itemView);
             mItemView = itemView;
-            mTextView = itemView.findViewById(R.id.item_task_title);
+            mTaskTitle = itemView.findViewById(R.id.item_task_title);
+            mTaskStartDate = itemView.findViewById(R.id.task_start_date);
+            mTaskDueDate = itemView.findViewById(R.id.task_due_date);
+            mTaskOptions = itemView.findViewById(R.id.task_options_button);
+            mTaskOptions.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    PopupMenu popup = new PopupMenu(view.getContext(), mTaskOptions);
+                    popup.inflate(R.menu.menu_task_options);
+                    popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                        @Override
+                        public boolean onMenuItemClick(MenuItem item) {
+                            switch (item.getItemId()) {
+                                case R.id.action_mark_as_done:
+                                    //handle menu1 click
+                                    return true;
+                                case R.id.action_delete_task:
+                                    //handle menu2 click
+                                    return true;
+                                case R.id.action_color_task:
+                                    //handle menu3 click
+                                    return true;
+                                default:
+                                    return false;
+                            }
+                        }
+                    });
+                    popup.show();
+                }
+            });
             itemView.setOnClickListener(this);
         }
 
         void bind(int position) {
-            mTextView.setText(mData.get(position).getTaskTitle());
+            mTaskTitle.setText(mData.get(position).getTaskTitle());
+            mTaskStartDate.setText(AppUtils.getFriendlyDate(mData.get(position).getTaskStartDate()));
+            mTaskDueDate.setText(AppUtils.getFriendlyDate(mData.get(position).getTaskDueDate()));
             mItemView.setTag(mData.get(position).getTaskId());
         }
 
